850dcf26f69ec1aa1d753c9a285211bef437450c
aditya <ayuadi2845@gmail.com>
2025-09-24 23:16:07 +0530

    localhost to url

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -12,7 +12,7 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UserWidget = ({ userId, picturePath, isProfile = false }) => { 
+const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
   const { palette } = useTheme();
   const navigate = useNavigate();
@@ -55,7 +55,7 @@ const UserWidget = ({ userId, picturePath, isProfile = false }) => {
 
   return (
     <WidgetWrapper>
-      {/* FIRST ROW (Always visible) */}
+      {/* FIRST ROW */}
       <FlexBetween
         gap="0.5rem"
         pb="1.1rem"
